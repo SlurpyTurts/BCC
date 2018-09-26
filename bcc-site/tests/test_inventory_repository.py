9d007bf3fe21b904230fcb8271e1ab2c3a5470ae
SlurpyTurts <jordon.gerber@gmail.com>
2018-09-25 20:30:01 -0700

    overdue commit

--- bcc-site/tests/test_inventory_repository.py
+++ bcc-site/tests/test_inventory_repository.py
@@ -11,3 +11,23 @@ def test_get_basic_inventory():
     assert type(transaction_date) == datetime.date
     assert transaction_date.year == 2016
 
+def test_get_inv_for_part_by_inv():
+    inv_repo = inventory_repository.InventoryRepository()
+    parts = inv_repo.get_inv_parts_list()
+    inv_list = inv_repo.get_inv_location_id_list()
+    part_list_len = len(parts)
+    inv_max_count = len(inv_list)
+    count=0
+    array_len = part_list_len*inv_max_count;
+    results = [0 for x in range(array_len)]
+
+    for part in range(0, part_list_len):
+        for inv_count in range(0,inv_max_count):
+            part_number = parts[part]['partNumber']
+            inv_location = inv_list[inv_count]['id']
+            results[count] = inv_repo.get_inventory_values_by_location(part_number, inv_location)
+            print results
+            count = count + 1
+
+
+    assert len(results) < 100
--- bcc-site/tests/test_order_overview_repository.py
+++ bcc-site/tests/test_order_overview_repository.py
@@ -1,6 +1,6 @@
 from bcc.dataaccess import order_repository
 
 def test_get_order_list():
-    order_detail_repo = order_repository.OrderOverviewRepository()
-    results = order_detail_repo.get_order_overview()
+    order_repo=order_repository.OrderRepository()
+    results = order_repo.get_order_overview(21,20)
     assert len(results) == 51
