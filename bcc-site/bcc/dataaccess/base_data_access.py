7abff99aa6fab4fd405e1f2bb7afeb10ab07adc6
SlurpyTurts <jordon.gerber@gmail.com>
2019-02-27 17:06:13 -0800

    tried pulling, broke everything. reverted all local files to my previous commit and everything working locally.

---
+++ bcc-site/bcc/dataaccess/base_data_access.py
@@ -0,0 +1,21 @@
+import db_connection
+
+class BaseDataAccess:
+
+    def select_request(self, query, parameters=()):
+        connection = db_connection.get_connection()
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(query, parameters)
+                return cursor.fetchall()
+        finally:
+            connection.close()
+
+    def select_one(self, query, parameters=()):
+        connection = db_connection.get_connection()
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(query, parameters)
+                return cursor.fetchone()
+        finally:
+            connection.close()
